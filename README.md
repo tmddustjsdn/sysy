daaffac2ca34316f037837daf81e174753414a29
YOOseungyeon <zn3400@naver.com>
2024-06-08 01:34:43 +0900

    main.py 수정

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # sysy
+# 2022204004 유승연
+# 옵션 방명록 삭제 기능 추가함
+
+# animate라이브러리 가지고옴
+# self -> index.html/style.css/kkk.js
--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from starlette.requests import Request
 
 app = FastAPI()
 
-# Mounting static files
-app.mount("/static", StaticFiles(directory="test/self"), name="static")
+# 정적 파일을 제공하기 위해 StaticFiles 클래스를 사용하여 디렉토리를 설정합니다.
+app.mount("/static", StaticFiles(directory="self"), name="static")
 
-# Templates setting
-templates = Jinja2Templates(directory="test/self")
+# Jinja2Templates를 사용하여 템플릿을 설정합니다.
+templates = Jinja2Templates(directory="self")
 
-# Serve the index.html
+# index.html을 렌더링하는 라우터를 추가합니다.
 @app.get("/")
-async def read_root(request: Request):
+async def read_index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-# Serve a file directly for testing
-@app.get("/file")
-async def get_file():
-    return FileResponse("test/self/index.html")
+# kkk.js 파일을 제공하는 라우터를 추가합니다.
+@app.get("/kkk.js")
+async def read_kkk_js():
+    return FileResponse("self/kkk.js", media_type="application/javascript")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
